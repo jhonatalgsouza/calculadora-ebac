f13f26f53d3b4d7f9aeab6cdabdb719f781b0806
Jhonata Souza <jhonataleonardosouza@gmail.com>
2024-11-21 23:59:04 -0300

    commit

--- calculadora.sh
+++ calculadora.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 operacao = 0
+
 while operacao != 5:
   num1 = int(input('Insira um número: '))
   num2 = int(input('Insira outro número: '))
 
-  operacao = int(input('Escolha a operação desejada: \n 1 - Adição\n 2 - Subtração\n 3 - Multiplicação\n 4 - Divisão\n 5 - Sair\n'))
+  operacao = int(input('Escolha a operação desejada:\n 1 - Adição\n 2 - Subtração\n 3 - Multiplicação\n 4 - Divisão\n 5 - Sair\n'))
 
   if operacao == 1:
     print('A adição de', num1, 'e', num2, 'é', num1 + num2)
@@ -15,5 +18,5 @@ while operacao != 5:
     print('A divisão de', num1, 'e', num2, 'é', num1 / num2)
   elif operacao == 5:
     print('Saindo...')
-  else: 
+  else:
     operacao = int(input('Comando inválido! Insira novamente: '))
